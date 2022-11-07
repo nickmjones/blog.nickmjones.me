328107f93ffbe53c5bfd092589b773e75d84e869
nickmjones <hello@nickmjones.me>
2022-11-06 22:04:06 -0500

    Moar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,7 @@
 module ApplicationHelper
 
   def markdown(text)
-    options = [:hard_wrap, :autolink, :no_intra_emphasis, :fenced_code_blocks, :underline, :highlight,
-               :no_images, :filter_html, :safe_links_only, :prettify, :no_styles]
+    options = [:hard_wrap, :autolink, :no_intra_emphasis, :fenced_code_blocks, :underline, :highlight, :no_images, :safe_links_only, :prettify, :no_styles]
     Markdown.new(text, *options).to_html.html_safe
   end
   
