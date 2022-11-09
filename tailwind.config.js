527f898fe737c2d4c1e39d14a9a909bc7741cf02
nickmjones <hello@nickmjones.me>
2022-11-08 21:37:29 -0500

    Tufte-erized

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
   ],
   theme: {
+    extend: {
+      colors: {
+        'lightyellow':'#FFF8EB',
+      }
+    },
     fontFamily: {
       sans: ["League Spartan", "Helvetica", "sans-serif"],
-      serif: ["Source Serif Pro", "Times New Roman", "serif"]
+      serif: ["Source Serif Pro", "Times New Roman", "serif"],
     }
   }
-}
+};
