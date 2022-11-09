0b0a1f5f534533dc4b351d30026422fe22addf33
nickmjones <hello@nickmjones.me>
2022-11-08 21:10:11 -0500

    Not sure what I was working on...

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -9,8 +9,8 @@ class PostDashboard < Administrate::BaseDashboard
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     id: Field::Number,
-    content: Field::Text,
     title: Field::String,
+    content: Field::Text,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
   }.freeze
@@ -22,8 +22,8 @@ class PostDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
     id
-    content
     title
+    content
     created_at
   ].freeze
 
@@ -31,8 +31,8 @@ class PostDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
     id
-    content
     title
+    content
     created_at
     updated_at
   ].freeze
@@ -41,8 +41,8 @@ class PostDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    content
     title
+    content
   ].freeze
 
   # COLLECTION_FILTERS
